504b44dd3db7207bc7c5d5c3f7a7539e05b18f69
Matthew Blomquist <bluefox_mvb@hotmail.com>
2019-03-28 21:51:45 -0600

    Finished part 2 of client

--- SkeetClient/SkeetClient/client.cpp
+++ SkeetClient/SkeetClient/client.cpp
@@ -35,17 +35,52 @@ void main()
 	// inet_addr is depricated, so I should change this latter. 
 	server_address.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 
-	// get a message from the user
-	char message[SOCKET_BUFFER_SIZE];
-	gets_s(message, SOCKET_BUFFER_SIZE);
+	int8 buffer[SOCKET_BUFFER_SIZE];
+	int32 player_1;
+	int32 player_2;
 
-	int flags = 0;
-	// send the message to the server
-	if (sendto(sock, message, strlen(message), flags,
-		(SOCKADDR*)&server_address, sizeof(server_address)) == SOCKET_ERROR)
+	printf("type w, a, s, or d to move, q to quit\n");
+	bool32 is_running = 1;
+	while (is_running)
 	{
-		printf("sendto failed: %d", WSAGetLastError());
-		return;
+		// get input
+		scanf_s("\n%c", &buffer[0], 1);
+
+		// send to server
+		int buffer_length = 1;
+		int flags = 0;
+		SOCKADDR* to = (SOCKADDR*)&server_address;
+		int to_length = sizeof(server_address);
+		if (sendto(sock, buffer, buffer_length, flags, to, to_length) == SOCKET_ERROR)
+		{
+			printf("sendto failed: %d", WSAGetLastError());
+			return;
+		}
+
+		// wait for reply
+		flags = 0;
+		SOCKADD_IN from;
+		int from_size = sizeof(from);
+		int bytes_received = recvfrom(sock, buffer, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&from, &from_size);
+
+		if (bytes_received == SOCKET_ERROR)
+		{
+			printf("recvfrom returned SOCKET_ERROR, WSAGetLastError() %d", WSAGetLastError());
+			break;
+		}
+
+		// grab data from packet
+		int32 read_index = 0;
+
+		memcpy(&player_1, &buffer[read_index], sizeof(player_1));
+		read_index += sizeof(player_1);
+
+		memcpy(&player_2, &buffer[read_index], sizeof(player_2));
+		read_index += sizeof(player_2);
+
+		memcpy(&is_running, &buffer[read_index], sizeof(is_running));
+
+		printf("x:%d, y:%d, is_running:%d\n", player_1, player_2, is_running);
 	}
 
 	printf("done");
